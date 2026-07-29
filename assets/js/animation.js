09501e8462a78c36b249f8bed854928dec9d1c86
aldrda <aldrdah23@gmail.com>
2026-07-29 15:39:26 +0300

    Add scroll reveal functionality using IntersectionObserver

---
+++ assets/js/animation.js
@@ -0,0 +1,42 @@
+// ===============================
+// Scroll Reveal
+// ===============================
+
+
+const elements =
+document.querySelectorAll(
+".section, .hero-content, .skill-card, .project-card, .certificate-card"
+);
+
+
+
+const observer =
+new IntersectionObserver((entries)=>{
+
+
+entries.forEach(entry=>{
+
+
+if(entry.isIntersecting){
+
+
+entry.target.classList.add("show");
+
+
+}
+
+
+});
+
+
+},{
+threshold:0.15
+});
+
+
+
+elements.forEach(element=>{
+
+observer.observe(element);
+
+});
