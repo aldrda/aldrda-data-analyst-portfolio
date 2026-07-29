04c8b9e70a7b0b6449bc976754908350b15ac328
aldrda <aldrdah23@gmail.com>
2026-07-29 15:27:18 +0300

    Update loader.js
    Implement page loader functionality to hide loader on page load.

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,1 +1,31 @@
+// ===============================
+// Page Loader
+// ===============================
 
+
+window.addEventListener("load",()=>{
+
+
+    const loader =
+    document.querySelector(".loader");
+
+
+
+    if(loader){
+
+
+        loader.classList.add("hide");
+
+
+        setTimeout(()=>{
+
+            loader.remove();
+
+        },500);
+
+
+    }
+
+
+
+});
